9f315535d30a61564e34083179adff9b4ac90d26
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-10 18:34:55 -0600

    Add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+const bodyParser = require('body-parser');
+
 import Server from './server/server';
 import router from './router/router';
 
 const server= Server.init(8000);
 
+//add body-parser
+server.app.use(bodyParser.json());
+server.app.use(bodyParser.urlencoded({ extended: true }));
+//assign router
 server.app.use(router);
+
 
 server.start(
     ()=>console.log("Server is running on port",server.app.get('port'))
